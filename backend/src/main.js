9e87c188408a9d0003a20a1b14632674327573cf
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-22 20:30:18 -0500

    Add index store to main

--- backend/src/main.js
+++ backend/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import './index.css'
 import App from './App.vue'
+import store from "./components/store/index.js";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(store)
+    .mount('#app')
