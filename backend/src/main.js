82d0702481ec8dba79118cec5bb1f0e74d57746a
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-23 11:04:35 -0500

    Add router to main

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,7 +3,9 @@ import './style.css'
 import './index.css'
 import App from './App.vue'
 import store from "./store/index.js";
+import router from "./router/index.js";
 
 createApp(App)
     .use(store)
+    .use(router)
     .mount('#app')
