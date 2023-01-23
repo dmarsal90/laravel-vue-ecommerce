ac0933ad912640e56926ecf2bede951a9abe4557
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-23 10:55:41 -0500

    Upate store index

--- backend/src/main.js
+++ backend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import './index.css'
 import App from './App.vue'
-import store from "./components/store/index.js";
+import store from "./store/index.js";
 
 createApp(App)
     .use(store)
