d09e0954566fa362769e72b58a5178b999a5cd54
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-23 16:35:32 -0500

    Add request password route

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
+import RequestPassword from "../views/RequestPassword.vue";
 
 const routes = [
     {
@@ -12,7 +13,12 @@ const routes = [
         path: '/login',
         name: 'login',
         component: Login
-    }
+    },
+    {
+        path: '/request-password',
+        name: 'requestPassword',
+        component: RequestPassword,
+    },
 ];
 const router = createRouter({
     history: createWebHistory(),
