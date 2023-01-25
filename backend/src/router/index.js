b67cbe5b013847071a4711ed8b1276d97bda15f0
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-25 17:39:24 -0500

    Update routes

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -4,8 +4,35 @@ import Login from "../views/Login.vue";
 import RequestPassword from "../views/RequestPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
 import NotFound from "../views/NotFound.vue";
+import AppLayout from '../components/AppLayout.vue'
+import Products from "../views/Products/Products.vue";
 
 const routes = [
+    {
+        path: '/',
+        redirect: '/app'
+    },
+    {
+        path: '/app',
+        name: 'app',
+        redirect: '/app/dashboard',
+        component: AppLayout,
+        meta: {
+            requiresAuth: true
+        },
+        children: [
+            {
+                path: 'dashboard',
+                name: 'app.dashboard',
+                component: Dashboard
+            },
+            {
+                path: 'products',
+                name: 'app.products',
+                component: Products
+            },
+        ]
+    },
     {
         path: '/dashboard',
         name: 'dashboard',
@@ -35,6 +62,17 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.state.user.token) {
+        next({name: 'login'})
+    } else if (to.meta.requiresGuest && store.state.user.token) {
+        next({name: 'app.dashboard'})
+    } else {
+        next();
+    }
+
 })
 
 export default router;
