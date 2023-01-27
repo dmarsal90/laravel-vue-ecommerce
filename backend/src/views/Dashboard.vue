5401a831b10de17a230c5abb192a75d604eb7848
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-26 19:30:35 -0500

    Update dashboard component

--- backend/src/views/Dashboard.vue
+++ backend/src/views/Dashboard.vue
@@ -1,11 +1,26 @@
 <template>
-    <h1>Dashboard</h1>
+    <div class="mb-2 flex items-center justify-between">
+        <h1 class="text-3xl font-semibold">Dashboard</h1>
+
+    </div>
+
+
+
+
+
+
 </template>
 
-<script>
-export default {
-    name: "Dashboard"
-}
+<script setup>
+import {UsersIcon} from '@heroicons/vue/20/solid';
+
+
+import {computed, onMounted, ref} from "vue";
+import Spinner from "../components/core/Spinner.vue";
+import CustomInput from "../components/core/CustomInput.vue";
+
+
+
 </script>
 
 <style scoped>
