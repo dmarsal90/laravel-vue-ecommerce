eded91814226af279997948c164cf3db78be15a9
David Marsal Ivars <davidmarsalivars@gmail.com>
2023-01-27 15:08:43 -0500

    Update admin middleware

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Admin
 {
@@ -16,6 +17,11 @@ class Admin
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if (Auth::user() && Auth::user()->is_admin == 1) {
+            return $next($request);
+        }
+        return response([
+            'message' => 'You don\'t have permission to perform this action'
+        ], 403);
     }
 }
